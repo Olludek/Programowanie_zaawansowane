e1a087baf57656f8fbc97486629d50c17d4dfe13
Aleksandra Olszynska <ola.olszynska2001@gmail.com>
2022-11-06 19:15:02 +0100

    wer.1.4

--- 24_10_2022/main.cpp
+++ 24_10_2022/main.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 
 int main() {
-    float a, b, x;
+    float a, b, c, x, y,z;
     cout << " Program obliczajacy miejsca zerowe funkcji liniowej" << endl;
     cout << "Podaj a: ";
     cin >> a;
     cout << "Podaj b: ";
     cin >> b;
+    cout << "Podaj c: ";
+    cin >> c;
 
     if(a==0) {
         if(b==0) {
@@ -20,6 +22,11 @@ int main() {
         x=(-b)/a;
     cout << "Miejsce zerowe: " << x;
     }
+
+    cout << endl;
+    cout << "Postac kierunkowa funkcji liniowej" << endl;
+    y=(a*x)+b;
+    cout << "y= " << y;
 
 
     return 0;
