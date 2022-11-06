41cbe5c864fd57394d35ee7340979c638359c9de
Aleksandra Olszynska <ola.olszynska2001@gmail.com>
2022-11-06 19:30:24 +0100

    wer.2.0

--- 24_10_2022/main.cpp
+++ 24_10_2022/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main() {
-    float a, b, c, x, y,z;
+    float a, b, c, x, y, x1, x2, delta;
     cout << " Program obliczajacy miejsca zerowe funkcji liniowej" << endl;
     cout << "Podaj a: ";
     cin >> a;
@@ -28,6 +29,26 @@ int main() {
     y=(a*x)+b;
     cout << "y= " << y;
 
+    cout << endl;
+    cout << " Program obliczajacy miejsca zerowe funkcji kwadratowej" << endl;
+
+    if (a!=0) {
+        delta= (b*b)-4*a*c;
+        x1=(-b-sqrt(delta))/2*a;
+        x2=(-b+sqrt(delta))/2*a;
+
+        cout << "Delta " << delta << endl;
+
+        if (delta<0) {
+            cout << " Nie ma miejsc zerowych" << endl;
+        } else if (delta==0) {
+            cout << "Jedno miejsce zerowe: " << -b/2*a << endl;
+        } else {
+            cout << "Dwa miejsca zerowe: x1=" << x1 << " x2=" << x2 << endl;
+        }
+    } else {
+        cout << "A jest rowne 0" << endl;
+    }
 
     return 0;
 }
