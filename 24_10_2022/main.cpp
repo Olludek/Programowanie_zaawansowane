f71674a4e786c62ef1ba29c5624b7364cb4e0fae
Aleksandra Olszynska <ola.olszynska2001@gmail.com>
2022-11-06 18:57:45 +0100

    wer.1.0

--- 24_10_2022/main.cpp
+++ 24_10_2022/main.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    float a, b, x;
+    cout << " Program obliczajacy miejsca zerowe funkcji liniowej" << endl;
+    cout << "Podaj a: ";
+    cin >> a;
+    cout << "Podaj b: ";
+    cin >> b;
+
+    x=(-b)/a;
+    cout << "Miejsce zerowe: " << x;
+
+
     return 0;
 }
